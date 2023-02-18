b8dba8dc6b32a463ecba7f40c13be64e8d923842
Kn1ght16 <Danchenko0@mail.ru>
2023-02-18 16:11:34 +0300

    task1 13.1 add main, node, stack

---
+++ main.py
@@ -0,0 +1,15 @@
+from utils.node import Node
+from utils.stack import Stack
+
+
+def main():
+    n1 = Node(5, None)
+    print(n1)
+    stack = Stack()
+    stack.push('data1')
+    stack.push('data2')
+    stack.push('data3')
+
+
+if __name__ == "__main__":
+    main()
