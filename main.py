5c06df2ad69f5c2b8a1bf3200f28db0be1d45f4c
Kn1ght16 <Danchenko0@mail.ru>
2023-02-24 22:50:44 +0300

    task1 13.2 add pop

--- main.py
+++ main.py
@@ -19,7 +19,27 @@ def main():
     print(stack.top.next_node.next_node.data)
     print(stack.top.next_node.next_node.next_node)
 
+    stack = Stack()
+    stack.push('data1')
+    data = stack.pop()
+
+    # стэк стал пустой
+    print(stack.top)
+
+    # pop() удаляет элемент и возвращает данные удаленного элемента
+    print(data)
+
+    stack = Stack()
+    stack.push('data1')
+    stack.push('data2')
+    data = stack.pop()
+
+    # теперь последний элемента содержит данные data1
+    print(stack.top.data)
+
+    # данные удаленного элемента
+    print(data)
+
 
 if __name__ == "__main__":
     main()
-    input()
--- utils/stack.py
+++ utils/stack.py
@@ -13,11 +13,33 @@ class Stack:
         """top— последний элемент в стэке"""
         self.top = None
 
+    def __repr__(self) -> str:
+        """Возвращает атрибуты в понятном виде"""
+        text = ""
+        for dic in self.__dict__:
+            text += dic + "=" + str(self.__dict__[dic]) + ", "
+        return f"{text[:-2]}"
+
     def push(self, data) -> Node:
-        """функция добавляет элемент в  стэк"""
+        """push: добавить элемент в стек"""
         if len(self.datas) == 0:
             new_node = Node(data)
         else:
             new_node = Node(data, self.top)
         self.top = new_node
         return self.datas.append(new_node)
+
+    def pop(self) -> Node:
+        """pop: удалить  последний элемент из стека"""
+        if len(self.datas) == 0:
+            self.top = None
+        elif len(self.datas) == 1:
+            new_node = self.top
+            self.top = None
+            self.datas.pop()
+            return new_node.data
+        else:
+            new_node = self.top
+            self.top = self.datas[len(self.datas) - 2]
+            self.datas.pop()
+            return new_node.data
